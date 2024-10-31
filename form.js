028c09d2e1b8bab8e3f1214a5cf553cb2e2eb7ca
Simone Barani <baranisimone@itis-molinari.eu>
2024-10-31 12:24:22 +0100

    Modifica form

--- form.js
+++ form.js
@@ -15,7 +15,41 @@ const createForm = (parentElement) => {
             callback = callbackInput
         },
 
+        //document.getElementById(createPrenota).addEventListener("click", function() {
+        //    creaModale();
+        //});
         
+        creaModale:() =>{
+            // Aggiunge la struttura del modale direttamente all'interno del body
+            document.body.innerHTML += `
+                <div class="modal fade" id="dynamicModal" tabindex="-1">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title">Modal title</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <p>Modal body text goes here.</p>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-primary">Save changes</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            `;
+        
+            // Inizializzazione e visualizzazione del modale con Bootstrap
+            const modal = new bootstrap.Modal(document.getElementById('dynamicModal'));
+            modal.show();
+        
+            // Rimozione del modale dal DOM una volta chiuso
+            document.getElementById('dynamicModal').addEventListener('hidden.bs.modal', function () {
+                document.getElementById('dynamicModal').remove();
+            });
+        },
         
         render: () => {
             for (let key in data) {

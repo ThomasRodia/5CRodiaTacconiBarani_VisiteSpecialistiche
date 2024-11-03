106732f65cddf47b2e2b511d068cf899c2d26baf
Tacconi <tacconisimone@itis-molinari.eu>
2024-11-03 18:35:37 +0100

    modifica

--- modal.js
+++ modal.js
@@ -1,74 +1,77 @@
-
 function createBookingModal() {
-    
-    if (!document.getElementById("bookingModal")) {
-      const modalHTML = `
-        <div class="modal fade" id="bookingModal" tabindex="-1" role="dialog" aria-labelledby="bookingModalLabel" aria-hidden="true">
-          <div class="modal-dialog" role="document">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title" id="bookingModalLabel">Aggiungi Prenotazione</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-              <div class="modal-body">
-                <form id="bookingForm">
-                  <div class="form-group">
-                    <label for="appointmentDate">Data</label>
-                    <input type="date" class="form-control" id="appointmentDate" required>
-                  </div>
-                  <div class="form-group">
-                    <label for="appointmentTime">Ora</label>
-                    <select class="form-control" id="appointmentTime" required>
-                      <option value="">Seleziona un'ora</option>
-                      <option value="8">8:00</option>
-                      <option value="9">9:00</option>
-                      <option value="10">10:00</option>
-                      <option value="11">11:00</option>
-                      <option value="12">12:00</option>
-                    </select>
-                  </div>
-                  <div class="form-group">
-                    <label for="patientName">Nominativo</label>
-                    <input type="text" class="form-control" id="patientName" required>
-                  </div>
-                </form>
-              </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Annulla</button>
-                <button type="button" class="btn btn-primary" id="submitBooking">Invia</button>
-              </div>
+  if (!document.getElementById("bookingModal")) {
+    const modalHTML = `
+      <div class="modal fade" id="bookingModal" tabindex="-1" role="dialog" aria-labelledby="bookingModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="bookingModalLabel">Aggiungi Prenotazione</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <form id="bookingForm">
+                <div class="form-group">
+                  <label for="appointmentDate">Data</label>
+                  <input type="date" class="form-control" id="appointmentDate" required>
+                </div>
+                <div class="form-group">
+                  <label for="appointmentTime">Ora</label>
+                  <select class="form-control" id="appointmentTime" required>
+                    <option value="">Seleziona un'ora</option>
+                    <option value="8">8:00</option>
+                    <option value="9">9:00</option>
+                    <option value="10">10:00</option>
+                    <option value="11">11:00</option>
+                    <option value="12">12:00</option>
+                  </select>
+                </div>
+                <div class="form-group">
+                  <label for="patientName">Nominativo</label>
+                  <input type="text" class="form-control" id="patientName" required>
+                </div>
+              </form>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Annulla</button>
+              <button type="button" class="btn btn-primary" id="submitBooking">Invia</button>
             </div>
           </div>
         </div>
-      `;
-      document.body.insertAdjacentHTML('beforeend', modalHTML);
-    }
-  
-    
-    $('#bookingModal').modal('show');
-  
-    
-    document.getElementById("submitBooking").onclick = function () {
-      const date = document.getElementById("appointmentDate").value;
-      const time = document.getElementById("appointmentTime").value;
-      const patientName = document.getElementById("patientName").value;
-  
-      if (!date || !time || !patientName) {
-        alert("Per favore, compila tutti i campi.");
-        return;
-      }
-  
-      const dateKey = formatDate(new Date(date));
-      if (!appointmentCache[dateKey]) {
-        appointmentCache[dateKey] = {};
-      }
-      appointmentCache[dateKey][time] = patientName;
-  
-      $('#bookingModal').modal('hide'); 
-      document.getElementById("bookingForm").reset(); 
-      renderWeeklySchedule(); 
-    };
+      </div>
+    `;
+    document.body.insertAdjacentHTML('beforeend', modalHTML);
   }
-  
+
+  $('#bookingModal').modal('show');
+
+  document.getElementById("submitBooking").onclick = handleBookingSubmit;
+  document.getElementById("bookingForm").addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+      event.preventDefault(); // Previene il comportamento predefinito del form
+      handleBookingSubmit();
+    }
+  });
+}
+
+function handleBookingSubmit() {
+  const date = document.getElementById("appointmentDate").value;
+  const time = document.getElementById("appointmentTime").value;
+  const patientName = document.getElementById("patientName").value;
+
+  if (!date || !time || !patientName) {
+    alert("Per favore, compila tutti i campi.");
+    return;
+  }
+
+  const dateKey = formatDate(new Date(date));
+  if (!appointmentCache[dateKey]) {
+    appointmentCache[dateKey] = {};
+  }
+  appointmentCache[dateKey][time] = patientName;
+
+  $('#bookingModal').modal('hide'); 
+  document.getElementById("bookingForm").reset(); 
+  renderWeeklySchedule(); 
+}
--- table.js
+++ table.js
@@ -1,4 +1,4 @@
-const get='https://ws.cipiaceinfo.it/cache/get';
+const get = 'https://ws.cipiaceinfo.it/cache/get';
 const createTabella = (parentElement) => {
     let data = null;
     console.log(parentElement);
@@ -20,134 +20,113 @@ const createTabella = (parentElement) => {
             console.log("List data = ");
             console.log(listadata);
             let Row = "";
-            let key= Object.keys(listadata);//ottieni le chiavi del dizionario che dovrà essere formato da data###ora###nome
-            /*
-            listadata{
-            21/10/2024###8:rodia,
-            21/10/2024###9:tacconi,
-            21/10/2024###10:"",
-            22/10/2024###8:nobili }
-            */
-        
-           
+            let key = Object.keys(listadata); // ottieni le chiavi del dizionario che dovrà essere formato da data###ora###nome
+
             for (let i = 0; i < 5; i++) {
-             let valoreorariotabella=[];
-             for(let j=0;j<key.length;j++){
-            //    console.log("key[j] = "+key[j]);
-             let val=key[j].split("###");
-            // console.log("VAL = "+ val)
-            // console.log("VAL 1 = "+ val[1])
-             if(val[1]==hours[i]){
-                console.log("chiave = "+ key[j])
-                console.log("list data di key = "+listadata[key[j]]);
-             valoreorariotabella.push(listadata[key[j]]);
-             }
-           }
-           console.log(hours);
-           console.log(valoreorariotabella);
-           console.log("ciao");
-                let htmlRow = "<tr><td>" + hours[i] + "</td>" + "<td>"+valoreorariotabella[0]+"</td>" + "<td>"+valoreorariotabella[1]+"</td>" + "<td>"+valoreorariotabella[2]+"</td>"+ "<td>"+valoreorariotabella[3]+"</td>" + "<td>"+valoreorariotabella[4]+"</td>" + "</tr>" + "\n";
+                let valoreorariotabella = [];
+                for (let j = 0; j < key.length; j++) {
+                    let val = key[j].split("###");
+                    if (val[1] == hours[i]) {
+                        console.log("chiave = " + key[j]);
+                        console.log("list data di key = " + listadata[key[j]]);
+                        valoreorariotabella.push(listadata[key[j]]);
+                    }
+                }
+                console.log(hours);
+                console.log(valoreorariotabella);
+                let htmlRow = "<tr><td>" + hours[i] + "</td>" + "<td>" + (valoreorariotabella[0] || "") + "</td>" + "<td>" + (valoreorariotabella[1] || "") + "</td>" + "<td>" + (valoreorariotabella[2] || "") + "</td>" + "<td>" + (valoreorariotabella[3] || "") + "</td>" + "<td>" + (valoreorariotabella[4] || "") + "</td>" + "</tr>" + "\n";
                 Row += htmlRow;
-              
             }
             parentElement.innerHTML = header + Row + "</tbody></table>"; // Aggiungi le righe alla tabella
         },
-        
     };
 }
-
-
-
 
 console.log(document);
 
 let table = createTabella(document.getElementById("tabelle"));
-table.build(["LUNEDÌ", "MARTEDÌ", "MERCOLEDÌ", "GIOVEDÌ", "VENERDÌ"]);
-table.creaheader();
-let hours = ["8", "9", "10", "11", "12"];
-let test={
-    "21/10/2024###8":"Thomas",
-    "21/10/2024###9":"Thomas",
-    "21/10/2024###10":"Thomas",
-    "21/10/2024###11":"Thomas",
-    "21/10/2024###12":"Thomas",
-    "22/10/2024###8":"Thomas",
-    "22/10/2024###9":"Thomas",
-    "22/10/2024###10":"Thomas",
-    "22/10/2024###11":"Thomas",
-    "22/10/2024###12":"Thomas",
-    "23/10/2024###8":"Thomas",
-    "23/10/2024###9":"Thomas",
-    "23/10/2024###10":"Thomas",
-    "23/10/2024###11":"Thomas",
-    "23/10/2024###12":"Thomas",
-    "24/10/2024###8":"Thomas",
-    "24/10/2024###9":"Thomas",
-    "24/10/2024###10":"Thomas",
-    "24/10/2024###11":"Thomas",
-    "24/10/2024###12":"Thomas",
-    "25/10/2024###8":"Thomas",
-    "25/10/2024###9":"Thomas",
-    "25/10/2024###10":"Thomas",
-    "25/10/2024###11":"Thomas",
-    "25/10/2024###12":"Thomas",
 
-}
-//table.crea(test, hours); 
-/*
-let chiaviCache=Object.key(valcache);
-for(let i=0;i<chiaviCache.lenght;i++){
-    for(let j=0;j<;j++){
-    if(chiavi_dizionario[j])
+// Funzione per ottenere la data della settimana corrente
+const getDatesForWeek = (startDate) => {
+    let dates = [];
+    for (let i = 0; i < 5; i++) {
+        const date = new Date(startDate);
+        date.setDate(startDate.getDate() + i);
+        const giorno = ("0" + date.getDate()).slice(-2);
+        const mese = ("0" + (date.getMonth() + 1)).slice(-2);
+        const anno = date.getFullYear();
+        dates.push(`${giorno}/${mese}/${anno}`);
     }
-}
-    */
+    return dates;
+};
+
+// Ottieni la data attuale
+const oggi = new Date();
+let weekDates = getDatesForWeek(oggi);
+table.build(weekDates);
+table.creaheader();
+
+let hours = ["8", "9", "10", "11", "12"];
+let test = {
+    "21/10/2024###8": "Thomas",
+    "21/10/2024###9": "Thomas",
+    "21/10/2024###10": "Thomas",
+    "21/10/2024###11": "Thomas",
+    "21/10/2024###12": "Thomas",
+    "22/10/2024###8": "Thomas",
+    "22/10/2024###9": "Thomas",
+    "22/10/2024###10": "Thomas",
+    "22/10/2024###11": "Thomas",
+    "22/10/2024###12": "Thomas",
+    "23/10/2024###8": "Thomas",
+    "23/10/2024###9": "Thomas",
+    "23/10/2024###10": "Thomas",
+    "23/10/2024###11": "Thomas",
+    "23/10/2024###12": "Thomas",
+    "24/10/2024###8": "Thomas",
+    "24/10/2024###9": "Thomas",
+    "24/10/2024###10": "Thomas",
+    "24/10/2024###11": "Thomas",
+    "24/10/2024###12": "Thomas",
+    "25/10/2024###8": "Thomas",
+    "25/10/2024###9": "Thomas",
+    "25/10/2024###10": "Thomas",
+    "25/10/2024###11": "Thomas",
+    "25/10/2024###12": "Thomas",
+};
+
 const prendiDati = (myKey, myToken) => {
     return new Promise((resolve, reject) => {
-      fetch(get, {// da cambiare
-        method: "POST",
-        headers: {
-          "content-type": "application/json",
-          "key": myToken
-        },
-        body: JSON.stringify({
-          key: myKey
+        fetch(get, {
+            method: "POST",
+            headers: {
+                "content-type": "application/json",
+                "key": myToken
+            },
+            body: JSON.stringify({
+                key: myKey
+            })
         })
-      })
         .then(r => r.json())
         .then(r => {
-          const data = JSON.parse(r.result);
-          resolve(data);
+            const data = JSON.parse(r.result);
+            resolve(data);
         })
         .catch(error => reject(error));
     });
-  }
+};
 
-  async function creaDizionarioSettimana() {
-   
-    //let dizionario = {};
-    /*let valcache= prendiDati(myKey,myToken).then(dati=>{
-        
-        return dati;});////da sistemare 
-
-*/
-let valcache
-try {
-     valcache = await prendiDati(myKey, myToken);
-    console.log("Valcache aggiornato:", valcache);
+async function creaDizionarioSettimana() {
+    let valcache;
+    try {
+        valcache = await prendiDati(myKey, myToken);
+        console.log("Valcache aggiornato:", valcache);
+    } catch (error) {
+        console.error("Errore durante il recupero dei dati:", error);
+    }
     
-} catch (error) {
-    console.error("Errore durante il recupero dei dati:", error);
-}
-        let dizionario = {};
-    console.info("valcache = "+valcache)
-   /* let valcache={
-        "04/11/2024###8":"Pippo",
-        "07/11/2024###11":"Pippo"
-    };
-    */
-   console.log(valcache);
-   console.info("dopo valcache")
+    let dizionario = {};
+    console.info("valcache = " + valcache);
     let oggi = new Date();
     let giornoSettimana = oggi.getDay();
 
@@ -159,7 +138,6 @@ try {
         oggi.setDate(oggi.getDate() - (giornoSettimana - 1)); 
     }
 
-    
     for (let i = 0; i < 5; i++) {
         let giorno = ("0" + oggi.getDate()).slice(-2);
         let mese = ("0" + (oggi.getMonth() + 1)).slice(-2);
@@ -171,30 +149,26 @@ try {
             dizionario[chiave] = "";
         }
 
-        
         oggi.setDate(oggi.getDate() + 1);
     }
 
-
     for (let chiave in valcache) {
-        
         if (dizionario[chiave] !== undefined && valcache[chiave] !== "") {
-            
             dizionario[chiave] = valcache[chiave];
         }
     }
 
-console.log("prim dizz");    
-console.log (dizionario);
-console.log ("dopo dizz");
-    return dizionario;//rileva
-
+    console.log("prim dizz");    
+    console.log(dizionario);
+    console.log("dopo dizz");
+    return dizionario; // rileva
 }
-async function main(){
-let testa = await creaDizionarioSettimana();
-console.log("prima testa");
-console.log( testa);
-console.log("dopo testa");
-table.crea(testa, hours); 
+
+async function main() {
+    let testa = await creaDizionarioSettimana();
+    console.log("prima testa");
+    console.log(testa);
+    console.log("dopo testa");
+    table.crea(testa, hours); 
 }
 main();
